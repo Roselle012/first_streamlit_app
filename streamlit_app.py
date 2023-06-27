e391365223adcaa5c06549c2cc460962839579b3
Roselle012 <137844224+Roselle012@users.noreply.github.com>
2023-06-27 15:30:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+
 #New Section to display fruityvice.api response 
 streamlit.header('Fruityvice Fruit Advice!')
 
