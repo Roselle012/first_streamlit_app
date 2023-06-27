be4ee5640a94174bbac5081d603b37afa91b7470
Roselle012 <137844224+Roselle012@users.noreply.github.com>
2023-06-27 09:26:49 +0800

    Create streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,1 @@
 created the main python file
-
-import streamlit
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.title('My Parents New Healthy Diner')
-
-create the main python file
-create the main python file
