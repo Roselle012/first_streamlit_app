ab54b13f6496c1835079f8de91a7cb416e143f99
Roselle012 <137844224+Roselle012@users.noreply.github.com>
2023-06-27 20:03:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,27 +70,12 @@ try:
 except URLError as e:
   streamlit.error()
 
-#streamlit.text(fruityvice_response.json())
-#import snowflake.connector
-streamlit.header("View Our Fruit List - Add Your Favorites!")
-#Snowflake_related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-      my_cur.execute("select * from FRUIT_LOAD_LIST")
-      return my_cur.fetchall()
-import snowflake.connector
-streamlit.header("View Our Fruit List - Add Your Favorites :")
-#Snowflake-related functions
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
-
-#Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    my_cnx.close()
-    streamlit.dataframe(my_data_rows)
-
 import snowflakes.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
